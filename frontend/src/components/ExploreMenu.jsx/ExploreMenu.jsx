a5b27043cc6f9a41cefa21f3d8bf357a1f753fcc
TrinhThinh2004 <thinhtt2005@gmail.com>
2025-04-11 00:29:35 +0700

    build StoreContext and menu list

--- frontend/src/components/ExploreMenu.jsx/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu.jsx/ExploreMenu.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import "./ExploreMenu.css";
 import { menu_list } from "../../assets/assets";
-const ExploreMenu = () => {
+import { StoreContext } from "../../context/StoreContext";
+const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div className="explore-menu" id="explore-menu">
       <h1>Explore our menu </h1>`
@@ -11,8 +12,20 @@ const ExploreMenu = () => {
       </p>
       <div className="explore-menu-list">
         {menu_list.map((item, index) => (
-          <div className="explore-menu-list-item" key={index}>
-            <img src={item.menu_image} alt="" />
+          <div
+            onClick={() => {
+              setCategory((prev) =>
+                prev === item.menu_name ? "All" : item.menu_name
+              );
+            }}
+            className="explore-menu-list-item"
+            key={index}
+          >
+            <img
+              className={category === item.menu_name ? "active" : ""}
+              src={item.menu_image}
+              alt=""
+            />
             <p>{item.menu_name}</p>
             {/* <p>{item.description}</p> */}
             {/* <span>${item.price}</span> */}
