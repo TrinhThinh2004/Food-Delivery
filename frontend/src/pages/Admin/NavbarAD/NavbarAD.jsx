3d34da48855c46d4a8d0e23427e2f973c9c3d8b4
TrinhThinh2004 <thinhtt2005@gmail.com>
2025-05-12 22:05:41 +0700

    build logout page admin

--- frontend/src/pages/Admin/NavbarAD/NavbarAD.jsx
+++ frontend/src/pages/Admin/NavbarAD/NavbarAD.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import "./NavbarAD.css";
-import {assetsAD} from "../../../admin_assets/assetsAD.js";
-
-
+import { assetsAD } from "../../../admin_assets/assetsAD.js";
+import { assets } from "../../../assets/assets.js";
+import { useNavigate } from "react-router-dom";
+import { StoreContext } from "../../../context/StoreContext";
 const Navbar = () => {
+  const {setToken} = React.useContext(StoreContext);
+  const navigate = useNavigate(); 
+  const handleLogout = () => {
+        setToken("");
+        localStorage.removeItem("token");
+        localStorage.removeItem("isAdmin");
+        navigate("/");
+    }
   return (
     <div className="navbar">
-      <img className="logo" src={assetsAD.logo} alt="" />
-      <img className="profile" src={assetsAD.profile_image} alt="" />
+      <img className="logo" src={assetsAD.logo} alt="Logo" />
+      <div className="profile">
+        <img className="profile-image" src={assetsAD.profile_image} alt="Profile" />
+        <img onClick={handleLogout} className="logout" src={assets.logout_icon} alt="Logout" />
+      </div>
     </div>
   );
 };
