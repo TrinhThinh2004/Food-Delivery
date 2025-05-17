0cbf796682b2b41c6f4b247443f2ce014f606127
TrinhThinh2004 <thinhtt2005@gmail.com>
2025-05-17 23:08:58 +0700

    fix some thing

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -4,6 +4,6 @@ import authMiddleware from '../middleware/auth.js';
 
 const orderRouter = express.Router();
 orderRouter.post('/place', authMiddleware, placeOrder);
-orderRouter.post('/verify', authMiddleware, verifyOrder);
+orderRouter.post('/verify', verifyOrder);
 orderRouter.post('/userorders', authMiddleware, userOrders);
 export default orderRouter;
